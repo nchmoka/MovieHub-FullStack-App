2ef3977ebbcfae1b3915d68e0207191303f4cd1d
Yotam Aharon <yotiking2@gmail.com>
2024-03-23 17:32:02 +0200

    update MovieDetails and Home .js

--- frontend/src/components/MovieDetails.js
+++ frontend/src/components/MovieDetails.js
@@ -1,11 +1,14 @@
 import { useMoviesContext } from "../hooks/useMoviesContext";
 import { useAuthContext } from "../hooks/useAuthContext";
+import { useState } from "react";
+import { Slide, Fade } from "react-awesome-reveal";
+import { HiOutlineInformationCircle } from "react-icons/hi2";
 
 const MovieDetails = ({ movie }) => {
     const { dispatch } = useMoviesContext();
     const { user } = useAuthContext();
-
-    const handleRemoveMovieClick = async () => {
+    const [showDetails, setShowDetails] = useState(false);
+    const handleClick = async () => {
         if (!user) {
             return;
         }
@@ -19,6 +22,10 @@ const MovieDetails = ({ movie }) => {
         if (response.ok) {
             dispatch({ type: "DELETE_MOVIE", payload: json });
         }
+    };
+
+    const handleModalToggle = () => {
+        setShowDetails(!showDetails); // Toggle the state of the modal
     };
 
     // Updated function to handle adding a movie to the watchlist
@@ -47,34 +54,48 @@ const MovieDetails = ({ movie }) => {
         }
     };
 
-      // Updated function to handle adding a movie to the watchlist
-      const handleRemoveFromWatchlistClick = async () => {
-        if (!user) {
-            return;
-        }
-        // Include the user's email in the body of the request
-        const body = JSON.stringify({
-            userEmail: user.email, // Assuming 'user' object contains an 'email' field
-            movieId: movie._id // This might be redundant if you're already specifying the movie ID in the URL
-        });
-    };
 
     return (
-        <div className="movie-details" style={{ position: 'relative' }}>
-            <div style={{ position: 'absolute', right: '0', top: '0', display: 'flex', flexDirection: 'column', alignItems: 'flex-end', gap: '10px' }}>
-                <button className="material-symbols-outlined" onClick={handleRemoveMovieClick}>
-                    delete
-                </button>
-                <button title="Add to Watchlist" className="material-symbols-outlined" onClick={handleAddToWatchlistClick}>
+        <div className="movie-card bg-gray-100 rounded-lg overflow-hidden shadow-lg mb-4 w-80 relative flex flex-col group">
+
+            <img src={movie.image} alt={movie.title} className="w-full rounded-lg" />
+            <div className="absolute left-0 top-0 bottom-0 opacity-0 group-hover:opacity-100 p-4 w-full bg-black/60 group-hover:backdrop-blur-sm duration-500">
+                <div className="h-full flex flex-col justify-center text-white text-center">
+                  <Slide cascade>
+                    <h1 className="text-3xl font-bold mb-4">{movie.title}</h1>
+                    <div>
+                      <button className="bg-white text-black px-6 py-3 rounded-lg duration-300">
+                        <HiOutlineInformationCircle className="text-4xl"/>
+                      </button>
+                    </div>
+                  </Slide>
+                </div>
+              </div>
+            <button title="Add to Watchlist" className="material-symbols-outlined" onClick={handleAddToWatchlistClick}>
                     add 
-                </button>
+            </button>
+            {/* <div className="p-4 flex flex-col justify-between h-full">
+                <h4 className="text-xl font-semibold mb-2">{movie.title}</h4>
+                <p className="text-gray-700 mb-2"><strong>Genre: </strong>{movie.genre}</p>
+                <p className="text-gray-700 mb-2"><strong>Summary: </strong>{movie.summary}</p>
+                <p className="text-gray-700 mb-2"><strong>Director: </strong>{movie.director}</p>
+                <p className="text-gray-700 mb-2"><strong>Year: </strong>{movie.year}</p>
             </div>
-            <h4>{movie.title}</h4>
-            <p><strong>Genre: </strong>{movie.genre}</p>
-            <p><strong>Summary: </strong>{movie.summary}</p>
-            <p><strong>Director: </strong>{movie.director}</p>
-            <p><strong>Year: </strong>{movie.year}</p>
-            <p><strong>Image: </strong>{movie.image}</p>
+            <button onClick={handleModalToggle} className="bg-transparent absolute hover:border-opacity-100 bottom-2 right-2 px-4 py-4 rounded-full">
+                <HiOutlineInformationCircle className="text-3xl"/>
+            </button>
+            {showDetails && (
+                <div className="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex justify-center items-center z-50">
+                    <div className="bg-white p-8 rounded-lg">
+                        <h2 className="text-2xl font-bold mb-4">{movie.title}</h2>
+                        <p><strong>Genre: </strong>{movie.genre}</p>
+                        <p><strong>Summary: </strong>{movie.summary}</p>
+                        <p><strong>Director: </strong>{movie.director}</p>
+                        <p><strong>Year: </strong>{movie.year}</p>
+                        <button onClick={() => setShowDetails(false)} className="bg-red-500 text-white px-4 py-2 rounded-md mt-4">Close</button>
+                    </div>
+                </div>
+            )} */}
         </div>
     );
 };
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,16 @@
 import { useEffect } from "react";
+import React, { useState } from 'react'
+import {AiOutlineSearch} from 'react-icons/ai'
 import { useMoviesContext } from "../hooks/useMoviesContext";
 import { useAuthContext } from "../hooks/useAuthContext";
 // components
 import MovieDetails from "../components/MovieDetails";
-import MovieForm from "../components/MovieForm";
 
 const Home = () => {
     const { movies, dispatch } = useMoviesContext();
     const { user } = useAuthContext();
+    const [searchTerm, setSearchTerm] = useState('');
+    const [activeSearch, setActiveSearch] = useState([])
     useEffect(() => {
         const fetchMovies = async () => {
             const response = await fetch("/api/movies", {
@@ -16,7 +19,6 @@ const Home = () => {
                 },
             });
             const json = await response.json();
-
             if (response.ok) {
                 dispatch({ type: "SET_MOVIES", payload: json });
             }
@@ -25,16 +27,59 @@ const Home = () => {
             fetchMovies();
         }
     }, [dispatch, user]);
+    
+    const handleSearch = (e) => {
+        const term = e.target.value.trim().toLowerCase();
+        setSearchTerm(term);
+        if (term === '') {
+            setActiveSearch([]);
+        } else {
+            const searchResults = movies.filter(movie => movie.title.toLowerCase().includes(term));
+            setActiveSearch(searchResults);
+        }
+    };
+    
 
     return (
-        <div className="home">
-            <div className="movies">
-                {movies &&
-                    movies.map((movie) => (
+        <div className="movies">
+            <form className='w-[500px] relative'>
+                <div className="relative">
+                    <input type="search" placeholder='Type Here' className='w-full p-4 rounded-full bg-slate-800' onChange={handleSearch}/>
+                    <button type="button" className='absolute right-1 top-1/2 -translate-y-1/2 p-4 bg-slate-600 rounded-full'>
+                        <AiOutlineSearch />
+                    </button>
+                </div>
+                {/* {
+                    activeSearch.length > 0 && (
+                        <div className="absolute top-20 p-4 bg-slate-800 text-white w-full rounded-xl left-1/2 -translate-x-1/2 flex flex-col gap-2">
+                            {
+                                activeSearch.map(movie => (
+                                    <span key={movie._id}>{movie.title}</span>
+                                ))
+                            }
+                        </div>
+                    )
+                } */}
+            </form>
+            <div className="container mx-auto">
+                <div className="grid grid-cols-4 gap-10">
+                    {
+                    (searchTerm !== "" && activeSearch.length === 0) ? 
+                    (<div className="col-span-4 text-center text-gray-500">No results found.</div>)
+                    :
+                    (searchTerm === '' ? movies : activeSearch) &&
+                    (activeSearch.length > 0 ? activeSearch : movies).map((movie) => (
                         <MovieDetails key={movie._id} movie={movie} />
-                    ))}
+                    ))
+                    }
+                </div>
             </div>
-            <MovieForm />
+
+
+            {/* {movies &&
+                movies.map((movie) => (
+                    <MovieDetails key={movie._id} movie={movie} />
+            ))} */}
         </div>
     );
 };
